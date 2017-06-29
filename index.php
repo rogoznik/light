31fe2cee1ea08af702e028d475d58a1c37f91bbb
Nikita <nikrogoz@gmail.com>
2017-06-29 16:22:32 +0500

    create

---
+++ index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/style.css">
+    <script src="/js/lib/jquery-3.2.1.min.js"></script>
+    <script src="/js/script/script.js"></script>
+    <title>Управление светом</title>
+</head>
+<body>
+    <header class="header">
+
+    </header>
+
+    <div class="main">
+        <div class="content">
+            <div class="main-hall">
+                <div class="main-hall__zone">
+                    <div class="zone__sub-zone" data-num="1"></div>
+                    <div class="zone__sub-zone" data-num="2"></div>
+                </div>
+                <div class="main-hall__zone">
+                    <div class="zone__sub-zone" data-num="3"></div>
+                    <div class="zone__sub-zone" data-num="4"></div>
+                </div>
+                <div class="main-hall__zone">
+                    <div class="zone__sub-zone" data-num="5"></div>
+                    <div class="zone__sub-zone" data-num="6"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer class="footer">
+
+    </footer>
+</body>
+</html>
---
+++ js/script/script.js
@@ -0,0 +1,8 @@
+
+
+$(document).ready(function () {
+    var $zones = $('.zone__sub-zone');
+    $zones.on('click', function () {
+
+    });
+});
